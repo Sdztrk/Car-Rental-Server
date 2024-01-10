3323e89b0eb23c2dfe2a02401dda6a77139c043c
sdztrk <msaidozturk1@gmail.com>
2024-01-10 19:00:13 +0100

    Authantication middleware is updated

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -7,25 +7,20 @@ const jwt = require('jsonwebtoken')
 
 exports.isAuthenticated = (req, res, next) => {
 
-     console.log("Auth middleware running...")
-  
      const token = req.cookies
-     console.log("Tokennn:",token)
-     console.log("hey it is the request:",req)
-     
+     //console.log("Tokennn:",token)
 
+    const auth = req.headers?.authorization || null
+    const accessToken = auth ? auth.split(' ')[1] : null
 
-    // const auth = req.headers?.authorization || null
-    // const accessToken = auth ? auth.split(' ')[1] : null
+    req.isLogin = false
+    req.user = null
 
-    // req.isLogin = false
-    // req.user = null
-
-    // jwt.verify(accessToken, process.env.ACCESS_KEY, function (err, user) {
-    //     if (!err) {
-    //         req.isLogin = true
-    //         req.user = user
-    //     }
-    // })
+    jwt.verify(accessToken, process.env.ACCESS_KEY, function (err, user) {
+        if (!err) {
+            req.isLogin = true
+            req.user = user
+        }
+    })
     next()
 }
